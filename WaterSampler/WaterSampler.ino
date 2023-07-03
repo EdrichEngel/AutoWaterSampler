074406f494875fb6f3575eb97783edf1cbcbcce3
EDRICH-LAPTOP\Edrich <edrichengel@gmail.com>
2023-07-03 17:00:08 +0200

    Fixed some bugs and worked on csv file.

--- WaterSampler/WaterSampler.ino
+++ WaterSampler/WaterSampler.ino
@@ -14,16 +14,29 @@ float temperatureReading = 0;
 
 bool sdCardDetected = 0;
 bool sdCardDetectedPrev = 0;
+bool delayFlag = 0;
 
-int cur = 0;
+int sampleCurrent = 1;
+int curDelay = 0;
+int frequency = 1000;
+int sampleCount = 0;
 
-String sdData; 
+byte sdInit = 0;
+
+String sdDataWrite;
+String fileNameSettings = "/Settings.txt";
+String fileNameData = "/Data.txt";
+String sampleLocation = "N/A";
+String sampleDate = "N/A";
+String sampleCountString = "N/A";
+
+
 // Define Constants
 
 static const byte onboardLedPin = 2;
-static const byte turbidityPin = 12;
+static const byte turbidityPin = 27;
 static const byte servoPin = 13;
-static const byte temperaturePin = 15;
+static const byte temperaturePin = 4;
 static const byte cardDetectPin = 34;
 
 
@@ -41,11 +54,17 @@ static const byte portPos8 = 180;
 // Define Functions
 int servoDegrees(byte portNumber);
 
+bool testFileExistance(fs::FS &fs, String path);
+
 float getTurbidity(float rawADC);
 float getTemperature();
 
 void initSDCard();
-void appendFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, String path, String message);
+void readSettingsFile(fs::FS &fs, String path);
+void decodeSDString(String dataString);
+void splitStrings(String data);
+void writeGeneralInfoToSD();
 
 // Define Objects
 Servo valveServo;                               // Create servo object
@@ -53,15 +72,23 @@ OneWire oneWire(temperaturePin);                // Create a OneWire object for t
 DallasTemperature temperatureSensor(&oneWire);  // Create a DallasTemperature object by using the DallasTemperature library
 
 
+void IRAM_ATTR function_ISR() {
+  // Content of the function
+  delayFlag = 0;
+  Serial.println("Interupt!");
+}
+
+
 void setup() {
   // Start Serial Communication
   Serial.begin(115200);
 
   // Onboard LED setup
-   pinMode(onboardLedPin, OUTPUT);
+  pinMode(onboardLedPin, OUTPUT);
 
   // SD Card Breakout Board Setup
-   pinMode(cardDetectPin, INPUT);
+  pinMode(cardDetectPin, INPUT);
+  attachInterrupt(cardDetectPin, function_ISR, FALLING);
 
   // Allow allocation of timers
   ESP32PWM::allocateTimer(0);
@@ -79,51 +106,116 @@ void setup() {
   temperatureSensor.begin();
 
 
+  sdCardDetected = digitalRead(cardDetectPin);
+  sdCardDetectedPrev = sdCardDetected;
+
+  if (sdCardDetected) {
+    initSDCard();
+    if (!sdInit) {
+      delay(50);
+      initSDCard();
+    }
+  }
+
+
+
+  if (sdInit) {
+    readSettingsFile(SD, fileNameSettings);
+  }
+
+
+
+  if (!testFileExistance(SD, fileNameData)) {
+    Serial.println("Writing general info");
+    writeGeneralInfoToSD();
+  }
 }
 
 
 
-  
+
+
+
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if ((sampleCurrent <= sampleCount) || (sampleCount == 0)) {
 
-  sdCardDetected = digitalRead(cardDetectPin);
-  digitalWrite(onboardLedPin,sdCardDetected);
 
-  if (sdCardDetected != sdCardDetectedPrev){
-    if (sdCardDetected){
-      Serial.println("SD Card Detected");
-      initSDCard();
-    } else{
-      Serial.println("SD Card Removed");
+    if (delayFlag == 0) {
+
+      sdCardDetected = digitalRead(cardDetectPin);
+      digitalWrite(onboardLedPin, sdCardDetected);
+
+      if (sdCardDetected != sdCardDetectedPrev) {
+        if (sdCardDetected) {
+          Serial.println("SD Card Detected");
+          if (sdCardDetected) {
+            initSDCard();
+            if (!sdInit) {
+              delay(50);
+              initSDCard();
+            }
+          }
+
+          if (sdInit) {
+            readSettingsFile(SD, fileNameSettings);
+            sampleCurrent = 1;
+          }
+        } else {
+          Serial.println("SD Card Removed");
+          frequency = 1000;
+          sdInit = 0;
+          sampleCurrent = 1;
+          sampleCount = 0;
+        }
+      }
+      sdCardDetectedPrev = sdCardDetected;
+
+      turbidityReading = getTurbidity(analogRead(turbidityPin));
+      temperatureReading = getTemperature();
+      Serial.print("Sample ");
+      Serial.print(sampleCurrent);
+      Serial.print(" of ");
+      Serial.println(sampleCount);
+      Serial.print("Turbidity: ");
+      Serial.println(turbidityReading);
+      Serial.print("Temperature: ");
+      Serial.println(temperatureReading);
+      Serial.println(" ");
+
+      if (sdInit) {
+        sdDataWrite = sampleCurrent;
+        sdDataWrite += ",";
+        sdDataWrite += turbidityReading;
+        sdDataWrite += ",";
+        sdDataWrite += temperatureReading;
+        sdDataWrite += "\n";
+        appendFile(SD, fileNameData, sdDataWrite.c_str());
+      } else if (!sdInit && sdCardDetected) {
+        initSDCard();
+      }
+
+      sampleCurrent++;
+      delayFlag = 1;
+
+    } else if (delayFlag == 1) {
+
+      if (curDelay < frequency) {
+        delay(100);
+        curDelay += 100;
+      } else {
+        delayFlag = 0;
+        curDelay = 0;
+      }
     }
+  } else {
+    if (sampleCount != -1) {
+      Serial.println("System has finished sampling");
+    }
+    sampleCount = -1;
   }
-  sdCardDetectedPrev = sdCardDetected;
-
-  turbidityReading = getTurbidity(analogRead(turbidityPin));
-  temperatureReading = getTemperature();
-  Serial.print("Turbidity: ");
-  Serial.println(turbidityReading);
-  Serial.print("Temperature: ");
-  Serial.println(temperatureReading);
-
-  if(sdCardDetected){
-    sdData = turbidityReading;
-    sdData += ",";
-    sdData += temperatureReading;
-    sdData += "\n";
-    appendFile(SD, "/Data.txt", sdData.c_str());
-  }
-
-  
-
-
-  delay(5 * sToMs);
-  cur++;
-  valveServo.write(servoDegrees(cur));
-
 }
 
 
@@ -173,9 +265,13 @@ int servoDegrees(byte portNumber) {
 float getTurbidity(float rawADC) {
   float voltage;
   float turb;
+
+  //Serial.print("raw: ");
+  //Serial.println(rawADC);
   // Convert the raw ADC value to the output voltage from the sensor
-  voltage = (float)(rawADC * 5 / 4095.0*0.7015+0.8493);
-  Serial.println(voltage);
+  voltage = (float)(rawADC * 5 / 4095.0 * 0.8776 + 0.412);
+  /* Serial.print("VOLTAGE: ");
+  Serial.println(voltage);*/
   // Convert the output voltage from the sensor to turbidity in NTUs
   turb = (-1120.4 * voltage * voltage + 5742.3 * voltage - 4352.9);
   return turb;
@@ -187,35 +283,38 @@ float getTemperature() {
 }
 
 
-void initSDCard(){
+void initSDCard() {
   uint8_t sdCardType = SD.cardType();
   bool sdError = 0;
 
-  if(!SD.begin(32)){
+  if (!SD.begin(32)) {
     Serial.println("SD Card Mount Failed");
     sdError = 1;
     return;
   }
-  if(sdCardType == CARD_NONE){
+  Serial.println("SD Card Mounted");
+  if (sdCardType == CARD_NONE) {
     Serial.println("No SD card attached");
     sdError = 1;
     return;
+  } else {
+    sdInit = 1;
+    Serial.println("SD Card Attached");
   }
-
 }
 
 
 // Using function written for SD card example
-void appendFile(fs::FS &fs, const char * path, const char * message){
-  Serial.printf("Appending to file: %s\n", path);
+void appendFile(fs::FS &fs, String path, String message) {
+  //Serial.printf("Appending to file: %s\n", path.c_str());
 
-  File file = fs.open(path, FILE_APPEND);
-  if(!file){
+  File file = fs.open(path.c_str(), FILE_APPEND);
+  if (!file) {
     Serial.println("Failed to open file for appending");
     return;
   }
-  if(file.print(message)){
-      Serial.println("Message appended");
+  if (file.print(message.c_str())) {
+    //Serial.println("Message appended");
   } else {
     Serial.println("Append failed");
   }
@@ -223,3 +322,164 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
 }
 
 
+bool testFileExistance(fs::FS &fs, String path) {
+
+  Serial.printf("Testing Existance Of: %s\n", path.c_str());
+
+  File file = fs.open(path.c_str());
+  if (!file) {
+    Serial.println("File Does Not Exists");
+    return 0;
+  } else {
+    Serial.println("File Exists");
+    return 1;
+  }
+}
+
+
+void readSettingsFile(fs::FS &fs, String path) {
+  String sdDataRead;
+
+  //initSDCard();
+
+  Serial.printf("Reading Settings From: %s\n", path.c_str());
+
+  File file = fs.open(path.c_str());
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  while (file.available()) {
+    sdDataRead = file.readString();
+    splitStrings(sdDataRead);
+    //Serial.print(sdDataRead);
+  }
+  file.close();
+
+  if (!testFileExistance(SD, fileNameData)) {
+    Serial.println("Writing general info");
+    writeGeneralInfoToSD();
+  }
+}
+
+
+void splitStrings(String data) {
+  byte newLineReady = 1;
+  int newLineCharPos = 1;
+  int prevLineCharPos = 0;
+
+  while (newLineReady) {
+    newLineCharPos = data.indexOf('\n', newLineCharPos + 1);
+    /* Serial.print("Char found at: ");
+    Serial.println(newLineCharPos);*/
+    if (newLineCharPos == -1) {
+      newLineReady = 0;
+      newLineCharPos = data.length();
+    }
+
+
+    decodeSDString(data.substring(prevLineCharPos, newLineCharPos));
+    /* Serial.print("Start Line: ");
+    Serial.println(data.substring(prevLineCharPos, newLineCharPos));
+    Serial.print("End Line: ");*/
+    prevLineCharPos = newLineCharPos + 1;
+  }
+}
+
+void decodeSDString(String dataString) {
+  int unitFactor, temp, newLineCharPos;
+  String unitString, durationString;
+
+  if (strcmp(dataString.substring(0, 8).c_str(), "FileName") == 0) {
+    // File Name
+    fileNameData = dataString.substring(9, dataString.length());
+    Serial.print("File Name Changed To: ");
+    fileNameData = "/" + fileNameData;
+    Serial.println(fileNameData);
+
+  } else {
+    if (strcmp(dataString.substring(0, 9).c_str(), "Frequency") == 0) {
+      // Frequency
+      durationString = dataString.substring(10, dataString.length() - 2);
+      unitString = dataString.substring(dataString.length() - 2, dataString.length() - 1);
+
+
+
+      temp = durationString.toInt();
+
+      if ((strcmp(unitString.c_str(), "s") == 0) || (strcmp(unitString.c_str(), "S") == 0)) {
+        unitFactor = 1 * sToMs;
+      } else {
+        if ((strcmp(unitString.c_str(), "m") == 0) || (strcmp(unitString.c_str(), "M") == 0)) {
+          unitFactor = 60 * sToMs;
+        } else {
+          if ((strcmp(unitString.c_str(), "h") == 0) || (strcmp(unitString.c_str(), "H") == 0)) {
+            unitFactor = 3600 * sToMs;
+          }
+        }
+      }
+
+
+
+      frequency = temp * unitFactor;
+
+      Serial.print("Frequency Changed To: ");
+      Serial.println(frequency);
+    } else {
+      if (strcmp(dataString.substring(0, 8).c_str(), "Location") == 0) {
+        sampleLocation = dataString.substring(9, dataString.length()-1);
+        Serial.print("Sample Location: ");
+        Serial.println(sampleLocation);
+      } else {
+        if (strcmp(dataString.substring(0, 4).c_str(), "Date") == 0) {
+          sampleDate = dataString.substring(5, dataString.length()-1);
+          Serial.print("Sample Date: ");
+          Serial.println(sampleDate);
+        } else {
+          if (strcmp(dataString.substring(0, 11).c_str(), "SampleCount") == 0) {
+            sampleCountString = dataString.substring(12, dataString.length());
+            sampleCount = sampleCountString.toInt();
+            Serial.print("Sample Count: ");
+            Serial.println(sampleCount);
+          } else {
+            Serial.println("Error occured while decoding the settings file.");
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+void writeGeneralInfoToSD() {
+
+  sdDataWrite = "Location,";
+  sdDataWrite += sampleLocation;
+  sdDataWrite += ",";
+  sdDataWrite += "\n";
+  appendFile(SD, fileNameData, sdDataWrite);
+  sdDataWrite = "Date,";
+  sdDataWrite += sampleDate;
+  sdDataWrite += ",";
+  sdDataWrite += "\n";
+  appendFile(SD, fileNameData, sdDataWrite);
+  sdDataWrite = "Frequency,";
+  sdDataWrite += frequency;
+  sdDataWrite += ",";
+  sdDataWrite += "\n";
+  appendFile(SD, fileNameData, sdDataWrite);
+  sdDataWrite = "Sample Count,";
+
+  if (sampleCount == 0) {
+    sampleCountString = "N/A";
+  }
+  sdDataWrite += sampleCountString;
+  sdDataWrite += ",";
+  sdDataWrite += "\n";
+  appendFile(SD, fileNameData, sdDataWrite);
+  sdDataWrite = "SampleNumber,Turbidity,Temperature\n";
+  appendFile(SD, fileNameData, sdDataWrite);
+
+}
